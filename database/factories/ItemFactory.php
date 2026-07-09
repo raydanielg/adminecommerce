b7f56c7d5527812a44c04d54ebf07554c6575ef1
raydanielg <airezra2+1@gmail.com>
2026-07-09 05:52:17 +0300

    commit

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use App\Models\Item;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class FoodFactory extends Factory
+class ItemFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
